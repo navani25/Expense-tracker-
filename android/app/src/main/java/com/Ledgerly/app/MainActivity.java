c133fded9022af851461009601b2c731a6cd9173
Navaneethan <navaniram13@gmail.com>
2025-11-02 19:33:12 +0530

    Auto-sync: file changes

---
+++ android/app/src/main/java/com/Ledgerly/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.Ledgerly.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,11 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ledgerly.app',
-  appName: 'ledgerly',
-  webDir: 'dist',
+  appId: 'com.Ledgerly.app', // <-- Your unique app ID
+  appName: 'Ledgerly',       // <-- Your app's name
+  webDir: 'dist',            // <-- The folder where your web app is built (Vite uses 'dist')
   server: {
     androidScheme: 'https'
-  },
-  plugins: {
-    GoogleAuth: {
-      scopes: ['profile', 'email'],
-      serverClientId: "381448198833-4eao4v7ikh0k2o7pk0sbmpuhfgi9vouk.apps.googleusercontent.com",
-      androidClientId: "381448198833-k10nqmcf7k7rj4ah1ru1jjkn4oi3bbvu.apps.googleusercontent.com",
-      forceCodeForRefreshToken: true
-    }
   }
 };
 
